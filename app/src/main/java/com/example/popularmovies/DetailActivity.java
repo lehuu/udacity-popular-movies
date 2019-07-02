2f57f7261a55b9d218c2b13b2d6ac9a6a980f7db
Phuoc Le <phuoc.le@tum.de>
2019-07-02 12:49:52 +0200

    Migrate to androidx

--- app/src/main/java/com/example/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/popularmovies/DetailActivity.java
@@ -1,10 +1,10 @@
 package com.example.popularmovies;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
