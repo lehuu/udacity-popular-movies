5828541532d138104da25114bfc9abd147deebb6
Phuoc Le <phuoc.le@tum.de>
2019-07-03 19:48:06 +0200

    Decrease poster size

--- app/src/main/java/com/example/popularmovies/Utils/NetworkUtils.java
+++ app/src/main/java/com/example/popularmovies/Utils/NetworkUtils.java
@@ -14,7 +14,7 @@ public class NetworkUtils {
     private final static String API_KEY = BuildConfig.ApiKey;
     private final static String API_PARAM = "api_key";
     private final static String BASE_URL = "https://api.themoviedb.org/3/";
-    public static final String POSTER_URL = "https://image.tmdb.org/t/p/w500";
+    public static final String POSTER_URL = "https://image.tmdb.org/t/p/w342";
 
     private static Retrofit retrofit;
 
